dd9921da7e91de6e7ee1e07bd9c173023647869c
Justin <yundongkim2023@u.northwestern.edu>
2025-03-09 12:53:48 -0400

    adding chmod because of permission denied error.txt

--- hello_world.sh
+++ hello_world.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # This script is a sample script just to test you can run a bash script
+
+chmod +x /Users/justin/Documents/msds434/ci_cd_actions/hello_world.sh
 echo "This is a bash script, $0, running."
 echo "Hello World!!!"
